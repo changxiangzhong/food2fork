4831a81630fd28d568e764460d27e584cc0b4ed5
Xiangzhong Chang <chang@hyper.no>
2016-12-01 11:55:33 +0100

    Configure a SearchView for searching the recipe

--- app/src/main/java/com/xzchang/food2fork/RecipieListActivity.java
+++ app/src/main/java/com/xzchang/food2fork/RecipieListActivity.java
@@ -1,7 +1,12 @@
 package com.xzchang.food2fork;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class RecipieListActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class RecipieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipie_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recipie_list, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(RecipieListActivity.this, "Search", Toast.LENGTH_LONG).show();
+                MenuItemCompat.collapseActionView(searchItem);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
     }
 }
