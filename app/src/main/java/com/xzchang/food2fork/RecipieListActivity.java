73c519b06d2e3cf169f7ab28ac5400a847fbee1b
Xiangzhong Chang <chang@hyper.no>
2016-12-01 11:09:31 +0100

    Android studio generated project

---
+++ app/src/main/java/com/xzchang/food2fork/RecipieListActivity.java
@@ -0,0 +1,13 @@
+package com.xzchang.food2fork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipie_list);
+    }
+}
